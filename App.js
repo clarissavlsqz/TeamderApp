079853903ebca3b3bea7e7353cad57a235c32b10
clarissavlsqz <clarissa.a.velasquez@gmail.com>
2023-03-16 23:50:51 -0600

    create initial view

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import InitialView from "./InitialView";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Quicksand: require("./assets/fonts/Quicksand-VariableFont_wght.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    //<View style={styles.container}>
+    //  <Text>Open up App.js to start working on your app!</Text>
+    //  <StatusBar style="auto" />
+    //</View>
+    <SafeAreaView style={styles.container} onLayout={onLayoutRootView}>
+      <InitialView />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#2B2343",
+    justifyContent: "center",
   },
 });
