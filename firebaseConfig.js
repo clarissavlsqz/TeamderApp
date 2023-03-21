d70339e9c39e30785384f6a72bf96d498d3d9ea7
clarissavlsqz <clarissa.a.velasquez@gmail.com>
2023-03-21 00:37:59 -0600

    initialize firebase sdk

---
+++ firebaseConfig.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD2oeAng0twdBjtEc_4HpEYglh7Dx0frgw",
+  authDomain: "teamder-f776c.firebaseapp.com",
+  projectId: "teamder-f776c",
+  storageBucket: "teamder-f776c.appspot.com",
+  messagingSenderId: "693221137509",
+  appId: "1:693221137509:web:6e7cf64d567a1f9ed93a8c",
+  measurementId: "G-73BE1WHE49",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
