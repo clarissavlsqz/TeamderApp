aa70258566aa8a62d65c8e97895526a9b474ca4e
clarissavlsqz <clarissa.a.velasquez@gmail.com>
2023-03-23 17:47:05 -0600

    create profile ui skeleton

--- views/ProfileView.js
+++ views/ProfileView.js
@@ -1,9 +1,61 @@
-import { SafeAreaView, StatusBar } from "react-native";
+import { collection, getDocs, where, query } from "firebase/firestore";
+import { useState, useEffect } from "react";
+import { SafeAreaView, StatusBar, StyleSheet, Text } from "react-native";
+import { auth, db } from "../firebaseConfig";
 
 export default function ProfileView() {
+  const [userName, setUserName] = useState("");
+  const [userEmail, setUserEmail] = useState("");
+  const [userPersonality, setUserPersonality] = useState("");
+
+  useEffect(() => {
+    async function fetchUserData() {
+      const user = auth.currentUser;
+      if (user !== null) {
+        setUserEmail(user.email);
+        const usersRef = collection(db, "users");
+        const q = query(usersRef, where("email", "==", userEmail));
+
+        const querySnapshot = await getDocs(q);
+        querySnapshot.forEach((doc) => {
+          setUserName(doc.get("firstName") + " " + doc.get("lastName"));
+          setUserPersonality(doc.get("personality"));
+        });
+      }
+    }
+    fetchUserData();
+  }, [userEmail, userName, userPersonality]);
+
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
       <StatusBar barStyle={"light-content"} />
+      <Text style={styles.profileText}>
+        {" "}
+        <Text style={styles.boldText}>Name:</Text> {userName}
+      </Text>
+      <Text style={styles.profileText}>
+        {" "}
+        <Text style={styles.boldText}>Email:</Text> {userEmail}
+      </Text>
+      <Text style={styles.profileText}>
+        {" "}
+        <Text style={styles.boldText}>Personality:</Text> {userPersonality}
+      </Text>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  profileText: {
+    fontSize: 20,
+    marginTop: 10,
+  },
+  boldText: {
+    fontWeight: "bold",
+  },
+});
