8a2b9298a7f78effd1e0b2330477d2dbcfb48e57
clarissavlsqz <clarissa.a.velasquez@gmail.com>
2023-05-09 23:15:21 -0600

    integrated Modal into view

--- src/views/main/teams/TeamsView.jsx
+++ src/views/main/teams/TeamsView.jsx
@@ -1,60 +1,84 @@
-import React, { useMemo } from "react";
+import React, { useMemo, useState } from "react";
 import { Card, Text, View } from "react-native-ui-lib";
 import { StatusBar } from "expo-status-bar";
 import { FlatList } from "react-native";
 import { EvilIcons } from "@expo/vector-icons";
 import { useClassContext } from "../../../context/class-context";
 import UserAvatar from "../../../components/UserAvatar";
+import TeammatesModal from "../../../components/TeammatesModal";
 
-const TeamItem = ({ item }) => (
-  <Card
-    key={item.id}
-    style={{
-      marginVertical: 8,
-      marginHorizontal: 16,
-      borderRadius: 25,
-      elevation: 5,
-    }}
-    onPress={() => console.log(item.members)}
-  >
-    <View padding-20>
-      <View row spread>
-        <Text text40 $textDefault>
-          {item.name}
+const TeamItem = ({ item, listMembers, checkItemPressed }) => {
+  const returnMembers = item.members;
+  const wasPress = true;
+
+  return (
+    <Card
+      key={item.id}
+      style={{
+        marginVertical: 8,
+        marginHorizontal: 16,
+        borderRadius: 25,
+        elevation: 5,
+      }}
+      // onPress={() => console.log(item.members)}
+      onPress={() => {
+        checkItemPressed(wasPress);
+        listMembers(returnMembers);
+      }}
+    >
+      <View padding-20>
+        <View row spread>
+          <Text text40 $textDefault>
+            {item.name}
+          </Text>
+
+          <EvilIcons name="arrow-right" color="black" size={32} />
+        </View>
+        <View row>
+          <Text text90>{item.class.name}</Text>
+          <Text text90 $textDefault>
+            {" "}
+            | {item.class.id}
+          </Text>
+        </View>
+
+        <Text text70 $textDefault marginV-6>
+          People ({item.members.length})
         </Text>
 
-        <EvilIcons name="arrow-right" color="black" size={32} />
+        <View row>
+          {item.members.map((member) => (
+            <View
+              key={member.userid}
+              style={{
+                marginRight: -6,
+              }}
+            >
+              <UserAvatar user={member.user} />
+            </View>
+          ))}
+        </View>
       </View>
-      <View row>
-        <Text text90>{item.class.name}</Text>
-        <Text text90 $textDefault>
-          {" "}
-          | {item.class.id}
-        </Text>
-      </View>
-
-      <Text text70 $textDefault marginV-6>
-        People ({item.members.length})
-      </Text>
-
-      <View row>
-        {item.members.map((member) => (
-          <View
-            key={member.userid}
-            style={{
-              marginRight: -6,
-            }}
-          >
-            <UserAvatar user={member.user} />
-          </View>
-        ))}
-      </View>
-    </View>
-  </Card>
-);
+    </Card>
+  );
+};
 
 const HomeView = () => {
   const { userGroups } = useClassContext();
+  const [modalVisible, setModalVisible] = useState(false);
+  const [members, setMembers] = useState([]);
+
+  const getMembers = (itemMembers) => {
+    setMembers(itemMembers);
+  };
+
+  const checkModalVisibility = (visibility) => {
+    setModalVisible(visibility);
+  };
+
+  const closeModal = () => {
+    setModalVisible(false);
+  };
 
   return (
     <View useSafeArea flex>
@@ -62,9 +86,24 @@ const HomeView = () => {
       <FlatList
         keyboardShouldPersistTaps="handled"
         data={userGroups}
-        renderItem={TeamItem}
+        renderItem={({ item }) => (
+          <TeamItem
+            item={item}
+            listMembers={getMembers}
+            checkItemPressed={checkModalVisibility}
+          />
+        )}
         keyExtractor={(item) => item.id}
       />
+      {modalVisible && (
+        <View>
+          <TeammatesModal
+            members={members}
+            isVisible={modalVisible}
+            closeModal={closeModal}
+          />
+        </View>
+      )}
     </View>
   );
 };
